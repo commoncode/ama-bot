fd1a991dbc07304f2e66e5dc01bc0c2d60896d1a
chen <zhangchen199102@gmail.com>
2019-01-11 14:00:48 +1100

    botkit setup

---
+++ routes/webhooks.js
@@ -0,0 +1,8 @@
+webhookRoutes = (webserver, slackController) => {
+  webserver.post('/slack/receive', (req, res) => {
+    res.status(200);
+    slackController.handleWebhookPayload(req, res);
+  });
+}
+
+module.exports = webhookRoutes;
--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
 
-dotenv.load();
+const server = (slackController) => {
+  const app = express();
+  const port = process.env.PORT;
+  
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+      extended: true
+  }));
+  
+  app.listen(port, function() {
+    console.log('Bot is listening on port ' + port);
+  });
 
-const app = express();
-const port = process.env.PORT;
+  // import all the pre-defined routes.
+  const normalizedPath = require("path").join(__dirname, "/routes");
+  require("fs").readdirSync(normalizedPath).forEach(file => {
+    require("./routes/" + file)(app, slackController);
+  });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
+  slackController.webserver = app;
+}
 
-app.listen(port, function () {
-  console.log('Bot is listening on port ' + port);
-});
+module.exports = server;
