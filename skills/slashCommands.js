bb8bd4cd9979261e6702cf5e4cc5865af8e89169
Phoebe Lew <phoebe@commoncode.io>
2019-02-13 15:57:43 +1100

    Handle leaderboard command

--- skills/slashCommands.js
+++ skills/slashCommands.js
@@ -5,7 +5,15 @@ const slashCommands = slackController => {
     switch (req.command) {
       case '/ama':
         bot.replyAcknowledge();
-        bot.whisper(req, MAIN_HELP_TEXT);
+        switch (req.text) {
+          case 'leaderboard':
+            bot.whisper(req, 'Leaderboard TBA');
+            break;
+          case 'help':
+          default:
+            bot.whisper(req, MAIN_HELP_TEXT);
+            break;
+        }
         break;
       default:
         bot.replyAcknowledge();
