861b6b4a21f253e4eac60ae8472b9004565aed57
phoebe-lew <lew.huiying@gmail.com>
2019-02-19 16:50:28 +1100

    [CCP-150] Get top teachers and display on leaderboard (#28)
    * Get top 3 teachers
    
    * Fix merge errors, fix plurals
    
    * Fix command matching

--- skills/slashCommands.js
+++ skills/slashCommands.js
@@ -28,7 +28,7 @@ const leaderboardHandler = async (bot, req) => {
   const oldestValidDate = moment().subtract(7, 'days');
 
   // select learning points for the last week
-  const res = await Message.query()
+  const learnRes = await Message.query()
     .select('people.username', raw('COUNT(points.person_id)'))
     .from('messages')
     .where('messages.datetime', '>=', oldestValidDate.format())
@@ -37,6 +37,26 @@ const leaderboardHandler = async (bot, req) => {
     .where('points.teach', false)
     .groupBy('people.username')
     .orderBy('count');
+
+  const teachRes = await Message.query()
+    .select('people.username', raw('COUNT(points.person_id)'))
+    .from('messages')
+    .where('messages.datetime', '>=', oldestValidDate.format())
+    .joinRelation('points')
+    .join('people', 'points.person_id', 'people.id')
+    .where('points.teach', true)
+    .groupBy('people.username')
+    .orderBy('count');
+
+  const message = `${getMessageFromRes(learnRes, true)}
+  
+  ${getMessageFromRes(teachRes, false)}`;
+
+  bot.replyPrivateDelayed(req, message);
+};
+
+const getMessageFromRes = (res, learn) => {
+  if (res.length < 1) return '';
 
   const uniqueSortedPoints = [...new Set(res.map(row => row.count))];
 
@@ -48,22 +68,30 @@ const leaderboardHandler = async (bot, req) => {
     uniqueSortedPoints.length >= 3 &&
     res.filter(row => row.count === uniqueSortedPoints[2]);
 
-  let message = `*:tada: Congratulations :tada: to our best learners this week*:
+  let message = `*:tada: Congratulations :tada: to our best ${
+    learn ? 'learners' : 'teachers'
+  } this week*:
 
-  *${firstPlace[0].username}* learned *${uniqueSortedPoints[0]}* things`;
+  *${firstPlace[0].username}* ${learn ? 'learned' : 'taught'} *${
+  uniqueSortedPoints[0]
+}* ${uniqueSortedPoints[0] === '1' ? 'thing' : 'things'}`;
 
   if (secondPlace) {
-    message += `*${secondPlace.map(obj => obj.username).join(',')}* learned *${
-      uniqueSortedPoints[1]
-    }* things`;
+    message += `*${secondPlace.map(obj => obj.username).join(',')}* ${
+      learn ? 'learned' : 'taught'
+    } *${uniqueSortedPoints[1]}* ${
+      uniqueSortedPoints[1] === '1' ? 'thing' : 'things'
+    }`;
   }
   if (thirdPlace) {
-    message += `*${thirdPlace.map(obj => obj.username).join(',')}* learned *${
-      uniqueSortedPoints[2]
-    }* things`;
+    message += `*${thirdPlace.map(obj => obj.username).join(',')}* ${
+      learn ? 'learned' : 'taught'
+    } *${uniqueSortedPoints[2]}* ${
+      uniqueSortedPoints[2] === '1' ? 'thing' : 'things'
+    }`;
   }
 
-  bot.replyPrivateDelayed(req, message);
+  return message;
 };
 
 module.exports = slashCommands;
