56c6218bd73144cb89f22b5f6975e5d16cb2f9e5
Phoebe Lew <phoebe@commoncode.io>
2019-02-28 12:23:10 +1100

    Allow messages without teacher, make skills case insensitive

---
+++ migrations/20190228115138_alter_skills_name_citext.js
@@ -0,0 +1,17 @@
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema
+      .raw('CREATE EXTENSION IF NOT EXISTS CITEXT')
+      .alterTable('skills', table => {
+        table.specificType('name', 'CITEXT').alter();
+      }),
+  ]);
+};
+
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.alterTable('User', table => {
+      table.string('name').alter();
+    }),
+  ]);
+};
