8394ae1dfbd87461fdcbb9d87d11c5ff8f0de25c
Ben Lomax <lomax.on.the.run@gmail.com>
2019-07-16 08:27:27 +0800

    Add logging for body and headers

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -1,5 +1,9 @@
 const webhookRoutes = (webserver, slackController) => {
   webserver.post('/slack/receive', (req, res) => {
+    console.log('Received request:');
+    console.log(req.body);
+    console.log('Received request headers:');
+    console.log(req.headers);
     res.status(200);
     slackController.handleWebhookPayload(req, res);
   });
