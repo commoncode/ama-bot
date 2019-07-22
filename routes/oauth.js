66db966687ed897e2001d69ea8ea53ed1a160f08
Ben Lomax <lomax.on.the.run@gmail.com>
2019-07-22 09:24:20 +0800

    Extra logging

--- routes/oauth.js
+++ routes/oauth.js
@@ -13,17 +13,24 @@ const authRoutes = (webserver, slackController) => {
     };
 
     slackApi.api.oauth.access(opts, (error, auth) => {
+      console.info('BENTEST: oauth started')
       if (error) {
+        console.info('BENTEST: error 1')
         console.error('Error confirming oauth: ', error);
       } else {
+        console.info('BENTEST: testing token...')
         slackApi.api.auth.test({ token: auth.access_token }, (error, identity) => {
 
+          console.info('BENTEST: in token test')
           if (error) {
+            console.info('BENTEST: error 2')
             console.error('Error confirming oauth: ', error);
           }
 
           auth.identity = identity;
+          console.info('BENTEST: oauth successful')
           slackController.trigger('oauth:success', [auth]);
+          console.info('BENTEST: oauth success trigger sent')
 
           // TODO: create a 'login_success.html' page and redirect to it.
         });
