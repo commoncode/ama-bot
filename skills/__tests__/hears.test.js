87a6a1739e8b7dac52c4fa9d799eb19fceb413ad
Tony Tamplin <tonytamps@gmail.com>
2019-07-31 07:49:33 +1000

    Rough implementation of what I meant in the review

--- skills/__tests__/hears.test.js
+++ skills/__tests__/hears.test.js
@@ -1,5 +1,7 @@
 const { helpHandler, extractMessageContents } = require('../hears');
 const { MAIN_HELP_TEXT } = require('../../static');
+const genAsyncBot = require('../../lib/asyncBot');
+const { teacher1, teacher2 } = require('../__fixtures__/teachers');
 
 jest.mock('../../lib/asyncBot');
 
@@ -7,20 +9,10 @@ const TEST_USER_ID = 'TEST_USER';
 const TEST_USER_NAME = 'TEST_USER_NAME';
 const LEARNER_OBJECT = { id: TEST_USER_ID, name: TEST_USER_NAME };
 
-const TEACHER_1_ID = 'TEACHER1';
-const TEACHER_1_NAME = 'TEACHER_1_NAME';
-const TEACHER_2_ID = 'TEACHER2';
-const TEACHER_2_NAME = 'TEACHER_2_NAME';
+const mockBot = { whisper: jest.fn() };
+const asyncBot = genAsyncBot();
 
-const mockBot = new (jest.fn())();
-mockBot.whisper = jest.fn();
-mockBot.asyncBotReturn = jest.fn(({ user }) => {
-  let name;
-  if (user === TEST_USER_ID) { name = TEST_USER_NAME; }
-  if (user === TEACHER_1_ID) { name = TEACHER_1_NAME; }
-  if (user === TEACHER_2_ID) { name = TEACHER_2_NAME; }
-  return Promise.resolve({ user: { name } });
-});
+afterEach(() => jest.clearAllMocks());
 
 const getMessage = text => ({
   event: { text },
@@ -42,6 +34,7 @@ describe('Test the helpHandler', () => {
 describe('Test extractMessageContent', () => {
   test('no skill or teacher mentioned', async () => {
     const message = getMessage('Message with no teacher and no skill');
+    asyncBot.api.users.info.mockResolvedValue({ user: { name: TEST_USER_NAME } });
 
     const result = await extractMessageContents(mockBot, message);
 
@@ -54,6 +47,7 @@ describe('Test extractMessageContent', () => {
 
   test('one skill and no teacher mentioned', async () => {
     const message = getMessage('Message with no teacher and one _skill_');
+    asyncBot.api.users.info.mockResolvedValue({ user: { name: TEST_USER_NAME } });
 
     const result = await extractMessageContents(mockBot, message);
 
@@ -66,6 +60,7 @@ describe('Test extractMessageContent', () => {
 
   test('two skills and no teacher mentioned', async () => {
     const message = getMessage('Message with no teacher and _two_ _skills_');
+    asyncBot.api.users.info.mockResolvedValueOnce({ user: { name: TEST_USER_NAME } });
 
     const result = await extractMessageContents(mockBot, message);
 
@@ -77,46 +72,50 @@ describe('Test extractMessageContent', () => {
   });
 
   test('no skill and one teacher mentioned', async () => {
-    const message = getMessage(`Message with no skill and <@${TEACHER_1_ID}>`);
+    const message = getMessage(`Message with no skill and <@${teacher1.id}>`);
+    asyncBot.api.users.info.mockResolvedValueOnce({ user: { name: teacher1.name } });
+    asyncBot.api.users.info.mockResolvedValueOnce({ user: { name: TEST_USER_NAME } });
 
     const result = await extractMessageContents(mockBot, message);
 
     expect(result).toMatchObject({
       learnerObject: LEARNER_OBJECT,
-      teacherObjects: [{ id: TEACHER_1_ID, teacherName: TEACHER_1_NAME }],
+      teacherObjects: [{ id: teacher1.id, teacherName: teacher1.name }],
       skills: [],
     });
   });
 
   test('no skill and two teachers mentioned', async () => {
-    const message = getMessage(
-      `Message with no skill and <@${TEACHER_1_ID}> and <@${TEACHER_2_ID}>`
-    );
+    const message = getMessage(`Message with no skill and <@${teacher1.id}> and <@${teacher2.id}>`);
+    asyncBot.api.users.info.mockResolvedValueOnce({ user: { name: teacher1.name } });
+    asyncBot.api.users.info.mockResolvedValueOnce({ user: { name: teacher2.name } });
+    asyncBot.api.users.info.mockResolvedValueOnce({ user: { name: TEST_USER_NAME } });
 
     const result = await extractMessageContents(mockBot, message);
 
     expect(result).toMatchObject({
       learnerObject: LEARNER_OBJECT,
       teacherObjects: [
-        { id: TEACHER_1_ID, teacherName: TEACHER_1_NAME },
-        { id: TEACHER_2_ID, teacherName: TEACHER_2_NAME },
+        { id: teacher1.id, teacherName: teacher1.name },
+        { id: teacher2.id, teacherName: teacher2.name },
       ],
       skills: [],
     });
   });
 
   test('two skills and two teachers mentioned', async () => {
-    const message = getMessage(
-      `Message with _two_ _skills_ and <@${TEACHER_1_ID}> and <@${TEACHER_2_ID}>`
-    );
+    const message = getMessage(`Message with _two_ _skills_ and <@${teacher1.id}> and <@${teacher2.id}>`);
+    asyncBot.api.users.info.mockResolvedValueOnce({ user: { name: teacher1.name } });
+    asyncBot.api.users.info.mockResolvedValueOnce({ user: { name: teacher2.name } });
+    asyncBot.api.users.info.mockResolvedValueOnce({ user: { name: TEST_USER_NAME } });
 
     const result = await extractMessageContents(mockBot, message);
 
     expect(result).toMatchObject({
       learnerObject: LEARNER_OBJECT,
       teacherObjects: [
-        { id: TEACHER_1_ID, teacherName: TEACHER_1_NAME },
-        { id: TEACHER_2_ID, teacherName: TEACHER_2_NAME },
+        { id: teacher1.id, teacherName: teacher1.name },
+        { id: teacher2.id, teacherName: teacher2.name },
       ],
       skills: ['two', 'skills'],
     });
